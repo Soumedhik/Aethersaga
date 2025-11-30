cd5595889b8935d38dd94f6a9a6fc58bee65348c
Soumedhik <sumitabharati@gmail.com>
2025-12-01 01:17:02 +0530

    Remove basePath for root deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Aethersaga',
+  basePath: '',
   trailingSlash: true,
   images: {
     unoptimized: true,
